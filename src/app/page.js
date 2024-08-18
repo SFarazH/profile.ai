f628b3135ae9c18602af060fbe2613719f7d2dc8
Faraz || فراز <75844866+SFarazH@users.noreply.github.com>
2024-08-18 09:05:14 +0530

    edit layout

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,17 @@ import { useEffect } from "react";
 import ProductCard from "@/components/ProductCard";
 
 export default function Home() {
-
-
   return (
-    <main className="flex flex-row gap-16 p-12">
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-    </main>
+    <>
+      <div className="px-6 mt-4">
+        <main className="flex flex-wrap justify-center sm:justify-between gap-4">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </main>
+      </div>
+    </>
   );
 }
