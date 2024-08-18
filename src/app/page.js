a7540d81f434cc09511bddf8beec59b3814a4832
Faraz || فراز <75844866+SFarazH@users.noreply.github.com>
2024-08-18 10:07:06 +0530

    edit product card

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,27 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ProductCard from "@/components/ProductCard";
+import axios from "axios";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  const getProducts = async () => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => setProducts(res.data))
+      .catch((e) => console.error(e));
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
   return (
     <>
       <div className="px-6 mt-4">
         <main className="flex flex-wrap justify-center sm:justify-between gap-4">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          {products?.map((product) => (
+            <ProductCard key={product.id} data = {product} />
+          ))}
         </main>
       </div>
     </>
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,51 +1,36 @@
 import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
 
-const ProductCard = () => {
+const ProductCard = ({ data }) => {
   return (
     <>
       <div className="group my-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
-        <a
-          className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl"
-          href="#"
-        >
+        <div className="relative mt-3 flex mx-auto h-72 overflow-hidden rounded-xl">
           <img
-            className="peer absolute top-0 right-0 h-full w-full object-cover"
-            src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+            className="peer relative right-0 h-full  object-cover top-0"
+            src={data.image}
             alt="product image"
           />
-        </a>
+        </div>
         <div className="mt-4 px-5 pb-5">
-          <a href="#">
-            <h5 className="text-xl tracking-tight text-slate-900">
-              Nike Air MX Super 2500 - Red
-            </h5>
-          </a>
+          <h5 className="text-xl tracking-tight text-slate-900">
+            {data.title}
+          </h5>
+
           <div className="mt-2 mb-5 flex items-center justify-between">
             <p>
-              <span className="text-3xl font-bold text-slate-900">$449</span>
-              <span className="text-sm text-slate-900 line-through">$699</span>
+              <span className="text-3xl font-bold text-slate-900">
+                ${data.price}
+              </span>
             </p>
           </div>
-          <a
-            href="#"
-            className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+          <div
+            className="cursor-pointer flex relative bottom-0 items-center gap-4 justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-md font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+            onClick={() => console.log(data)}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="mr-2 h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-              />
-            </svg>
+            <FaShoppingCart size={25} />
             Add to cart
-          </a>
+          </div>
         </div>
       </div>
     </>
